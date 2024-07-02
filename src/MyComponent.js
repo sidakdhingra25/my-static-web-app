36f6e06ba6ca761ad37929bf2e35519e427d39ab
sidak <gdsd1524@gmail.com>
2024-07-02 07:38:19 +0530

    Your commit message here

---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Hello, I am Sidak</h1>
+      <p>This is my static React web app</p>
+    </div>
+  );
+};
+
+export default MyComponent;
